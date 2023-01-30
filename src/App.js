32f491dedd6770a9d393b79f6e136cb9a2d7c7d1
chrisIyer <hellohotbox420@gmail.com>
2023-01-29 20:52:53 -0500

    drag and drop functionality implemented.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
-// import Header from "./components/Header";
-// import Main from "./components/Main";
-// import Nav from "./components/Nav";
-// import Footer from "./components/Footer";
-
-import BlogEntry from "./components/BlogEntry";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container  from 'react-bootstrap/Container';
+import { DndContext,closestCenter } from '@dnd-kit/core';
+import  { arrayMove, SortableContext,verticalListSortingStrategy } from '@dnd-kit/sortable';
+import { useState } from 'react';
+import Form from "./components/Form";
+import {SortableItem} from "./components/SortableItem"
 
 
 function App() {
+  const [dueDate, setDueDate] = useState(['Yesterday', "Today", 'Tomorrow']);
   return (
     <div className="App">
-      <><BlogEntry /></>
+      <><Form /></>
+      <DndContext
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+      >
+        <Container className='p-3' style={{"width":"50%"}} align="center"
+        >
+        <h3>Deadline</h3>
+        <SortableContext
+        items={dueDate}
+        strategy={verticalListSortingStrategy}
+        >
+
+          {dueDate.map(date=><SortableItem key={date} id={date}></SortableItem>)}
+
+
+        </SortableContext>
+        </Container>
+
+      </DndContext>
      
     </div>
   );
+  function handleDragEnd(event){
+    const {active, over}=event
+    console.log('Active: '+ active.id)
+    console.log('Over: '+ over.id)
+
+    if(active.id !== over.id) {
+      setDueDate((date)=> {
+        const activeIndex = date.indexOf(active.id)
+        const overIndex = date.indexOf(over.id)
+        console.log(arrayMove(date,activeIndex,overIndex))
+
+        return arrayMove(date,activeIndex,overIndex)
+
+      })
+    }
+
+
+
+  }
+
+
 }
 
 export default App;
---
+++ src/components/SortableItem.js
@@ -0,0 +1,30 @@
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import { Card } from 'react-bootstrap'
+import Form from './Form'
+
+export function SortableItem(props){
+     const {
+          attributes,
+          listeners,
+          setNodeRef,
+          transform, 
+          transition
+     } = useSortable({id: props.id})
+
+     const style = {
+          transform: CSS.Transform.toString(transform),
+          transition
+     }
+     return(
+          <div ref={setNodeRef} style = {style} {...attributes}{...listeners}>
+               <Card body className='m-3'>{props.id}</Card>
+               {/* <Card body className='m-3'>
+                    <Form />
+               </Card> */}
+               
+
+
+          </div>
+     )
+}
