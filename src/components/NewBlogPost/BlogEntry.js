175ca4751cdfe7f7f7df07bf889e1d2ccd1276b8
chrisIyer <hellohotbox420@gmail.com>
2023-01-30 10:13:51 -0500

    incorportating react

---
+++ src/components/NewBlogPost/BlogEntry.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import BlogTitle from './BlogTitle'
+import BlogBody from './BlogBody'
+import BlogAuthor from './BlogAuthor'
+
+const BlogEntry = () => {
+  return (
+    <div>
+     <BlogTitle />
+     <BlogBody />
+     <BlogAuthor />
+
+
+
+
+
+    </div>
+  )
+}
+
+export default BlogEntry
